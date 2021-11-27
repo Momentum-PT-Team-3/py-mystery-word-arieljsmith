466cb22f2522f0ea8d45492034c9930681655b6a
arieljsmith <arieljeansmith@gmail.com>
2021-11-27 14:56:28 -0800

    Add dict_transform function, start guess_prompt_loop

--- mystery-word-game.py
+++ mystery-word-game.py
@@ -2,15 +2,15 @@
 # F U N C T I O N S
 # =============================================================================
 
-def guess_match(computer_choice, guess):
+def guess_match(computer_chosen_word, guess):
     """
     Function that takes the computer's chosen word and user's guess as
-    arguments and returns a bool indicating whether or not that guess occurs
-    within the computer's chosen word. !!! Function assumes the guess has
-    already been checked for validity outside the function !!!
+    arguments and returns a boolean indicating whether or not that guess
+    occurs within the computer's chosen word. !!! Function assumes the
+    guess has already been checked for validity outside the function !!!
     """
 
-    for letter in computer_choice:
+    for letter in computer_chosen_word:
         if guess.lower() == letter:
             return True
 
@@ -32,20 +32,61 @@ def incorrect_guess_tracker(entry, prior_count):
     return current_count
 
 
+def dict_transform(computer_chosen_word):
+    """
+    Takes the word chosen by the computer and makes it into a dictionary.
+    """
+    computer_word_dict = {}
+
+    for letter in computer_chosen_word:
+        computer_word_dict[letter] = 1
+
+    for letter in computer_chosen_word:
+        computer_word_dict[letter] += 1
+
+    return computer_word_dict
+
+
+def guess_prompt_loop(computer_chosen_word):
+    """
+    Function that takes the computer's chosen word and repeats the guessing process until the computer's chosen word
+    is guessed or the number of incorrect guesses hits 8.
+    """
+    wrong_guesses = 0
+    entered_letters = 0
+
+    while (wrong_guesses < 8) and (entered_letters < len(computer_chosen_word)):
+        user_guess = input("Guess ONE letter: ")
+        wrong_guesses = incorrect_guess_tracker(guess_match(computer_chosen_word, user_guess), wrong_guesses)
+
+        if guess_match(computer_chosen_word, user_guess) is False:
+            print("Your guess is incorrect.")
+        else:
+            print("Your guess is correct!")
+            entered_letters += 1
+
+    return
+
+
 # =============================================================================
 # O T H E R  C O D E
 # =============================================================================
+
 # NOTE 1: Test word will be "try"
 # NOTE 2: Consider making a separate doc defining a constant for all English
 #         letters (upper AND lower? Likely unnecessary) that can be used to
 #         check against.
 
 chosen_word = "try"
-wrong_guesses = 0
-user_guess = input("The computer has chosen a word. Guess ONE of the letters \
-in that word: ")
 
-print(f"Your guess is {guess_match(chosen_word, user_guess)}.")
+print("The computer has chosen a word.")
 
-wrong_guesses = incorrect_guess_tracker(guess_match(chosen_word, user_guess), wrong_guesses)
-print(f"You have {wrong_guesses} incorrect guesses so far.")
+guess_prompt_loop(chosen_word)
+# wrong_guesses = 0
+# user_guess = input("The computer has chosen a word. Guess ONE of the letters \
+# in that word: ")
+
+# print(f"Your guess is {guess_match(chosen_word, user_guess)}.")
+
+# wrong_guesses = incorrect_guess_tracker(guess_match(chosen_word, user_guess), wrong_guesses)
+# print(f"You have {wrong_guesses} incorrect guesses so far.")
